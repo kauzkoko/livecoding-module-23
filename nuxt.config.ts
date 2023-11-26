0e60d4c6ca855317443077c0d832919507d4544d
kauzkoko <kamelkebab@tutanota.com>
2023-11-26 10:30:57 +0100

    fix: netlify deploy error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,47 +31,47 @@ export default defineNuxtConfig({
       },
     },
   },
-  // pwa: {
-  //   registerType: "autoUpdate",
-  //   manifest: {
-  //     name: "Nuxt Vite PWA",
-  //     short_name: "NuxtVitePWA",
-  //     theme_color: "#ffffff",
-  //     icons: [
-  //       {
-  //         src: "pwa-192x192.png",
-  //         sizes: "192x192",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "pwa-512x512.png",
-  //         sizes: "512x512",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "pwa-512x512.png",
-  //         sizes: "512x512",
-  //         type: "image/png",
-  //         purpose: "any maskable",
-  //       },
-  //     ],
-  //   },
-  //   workbox: {
-  //     globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-  //   },
-  //   client: {
-  //     installPrompt: true,
-  //     // you don't need to include this: only for testing purposes
-  //     // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-  //     periodicSyncForUpdates: 20,
-  //   },
-  //   devOptions: {
-  //     enabled: true,
-  //     suppressWarnings: true,
-  //     navigateFallbackAllowlist: [/^\/$/],
-  //     type: "module",
-  //   },
-  // },
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Nuxt Vite PWA",
+      short_name: "NuxtVitePWA",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: "module",
+    },
+  },
   app: {
     head: {
       script: [

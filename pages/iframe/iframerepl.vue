69552994dea68e7d99825bdcd81248129f2b8447
kauzkoko <kamelkebab@tutanota.com>
2023-12-19 15:11:35 +0100

    feat: simple strudel iframe message example

---
+++ pages/iframe/iframerepl.vue
@@ -0,0 +1,19 @@
+<template>
+  <ClientOnly>
+    <div>
+      <iframe
+        class="w-screen"
+        src="http://localhost:5173/"
+        frameborder="0"
+      ></iframe>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup>
+onMounted(() => {
+  window.addEventListener("message", function (event) {
+    console.log("Message from iframe A: ", event.data);
+  });
+});
+</script>

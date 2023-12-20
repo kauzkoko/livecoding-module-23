7befea13db744043af3f0edfc7a101e8ff839543
kauzkoko <kamelkebab@tutanota.com>
2023-12-20 16:06:37 +0100

    feat: a bit more elaborate

--- pages/websocket/simplevisuals.vue
+++ pages/websocket/simplevisuals.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <div fixed right-0 top-0 bg-red>
+    <div class="fixed right-0 top-0 bg-red" v-show="false">
       <div>{{ status }}</div>
       <div>{{ parsedData }}</div>
     </div>
-    <div w-screen bg-black h-screen>
-      <div text-white>yoyoyo</div>
+    <div w-screen bg-black h-screen grid grid-cols-3 overflow-hidden>
       <div
-        circle
+        v-for="n in 10"
+        class="circle duration-300"
         :style="{ width: parsedData?.value.s === 'bd' ? '200px' : '50px' }"
-      ></div>
+      >
+        <div
+          class="circle bg-blue w-30px"
+          :style="{ width: parsedData?.value.s === 'bd' ? '200px' : '50px' }"
+          v-for="n in 9"
+        ></div>
+      </div>
     </div>
   </div>
 </template>

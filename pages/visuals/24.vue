4fb7f41fcb98fadfe13594994ce787236c0b2967
kauzkoko <kamelkebab@tutanota.com>
2023-12-20 21:13:37 +0100

    feat: visuals

--- pages/visuals/24.vue
+++ pages/visuals/24.vue
@@ -1,7 +1,8 @@
 <template>
+  <div text-white>{{ parsed }}</div>
   <div class="wrapper grid grid-cols-4 dashy">
     <div v-for="n in 32" class="smallCircle bg-white">
-      <div class="smallerCircle bg-black">
+      <div class="smallerCircle bg-black transform-gpu scale-$scale">
         <div class="smallerCircle bg-blue ml-$mlleft"></div>
       </div>
     </div>
@@ -10,15 +11,23 @@
 
 <script setup>
 const mlleft = css("mlleft", "10px");
+const scale = css("scale", 1);
 const { data } = useWebSocket("ws://localhost:8081");
-watch(data, (newVal) => {
-  if (newVal) {
-    const parsedData = JSON.parse(newVal);
-    if (parsedData.s === "bd") {
-      mlleft.value = "0px";
-    } else {
-      mlleft.value = "10px";
-    }
+const parsed = computed(() => {
+  if (data.value) {
+    return JSON.parse(data.value);
+  }
+  return "~";
+});
+
+watch(parsed, (p) => {
+  if (p.value.s === "bd") {
+    mlleft.value = "0px";
+  } else if (p.value.s === "sd") {
+    scale.value = 1.1;
+  } else {
+    scale.value = 1;
+    mlleft.value = "50px";
   }
 });
 </script>

68310d83de91536d97a4cc49ad6c67e16e20c2fa
kauzkoko <kamelkebab@tutanota.com>
2023-12-21 01:22:21 +0100

    feat: qr scanner

--- pages/qr/qrsequencer.vue
+++ pages/qr/qrsequencer.vue
@@ -1,22 +1,35 @@
 <template>
-  <div>
-    <button @click="startReading">start reading</button>
+  <div w-screen h-screen>
     <video ref="videoElem" w-100vw />
-    <div fixed right-0 top-0 bg-white>{{ code }}</div>
-    <div fixed right-0 top-30px bg-yellow>{{ codeCounter }}</div>
+    <div fixed right-0 top-0>
+      <div bg-blue-300>qr id: {{ code }}</div>
+      <div bg-blue-500>read count: {{ codeCounter }}</div>
+      <div bg-blue-700>toggle count: {{ toggleCounter }}</div>
+    </div>
+    <div
+      v-if="show"
+      @click="startReading()"
+      class="fixed top-50vh left-50vw transform-gpu translate--50% bg-white rounded-full w-80vw aspect-1 flexCenter font-sans text-1.5em"
+    >
+      <div>press and allow to start</div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import QrScanner from "qr-scanner";
 const { sendQrData } = useQrSequencer();
+setPageLayout("none");
 
+const show = ref(true);
 const videoElem = ref();
 const qrScanner = ref();
 const code = ref("");
 const codeCounter = ref(0);
+const toggleCounter = ref(0);
 const sendQrThrottled = useThrottleFn((qrData) => {
   sendQrData.value(qrData);
+  toggleCounter.value++;
 }, 2000);
 
 onMounted(() => {
@@ -32,5 +45,7 @@ onMounted(() => {
 });
 const startReading = () => {
   qrScanner.value.start();
+  show.value = false;
+  console.log(show.value);
 };
 </script>

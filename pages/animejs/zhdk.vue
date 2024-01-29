a7ab6484d847cb9031fa4219580301b417552a22
kauzkoko <kamelkebab@tutanota.com>
2024-01-29 19:12:22 +0100

    feat: add anime stuff

---
+++ pages/animejs/zhdk.vue
@@ -0,0 +1,25 @@
+<template>
+  <div w-screen h-screen class="font-sans">
+    <Zhdksvg />
+  </div>
+</template>
+
+<script setup>
+import { animate, stagger } from "@juliangarnierorg/anime-beta";
+
+onMounted(() => {
+  animate("#logo", {
+    translateY: { to: -3000 },
+    duration: 4000,
+    ease: "easein",
+  });
+
+  animate("#energyball", {
+    keyframes: [
+      { translateY: 200, duration: 500 },
+      { radius: 3, duration: 250, delay: 400 },
+    ],
+    ease: "out",
+  });
+});
+</script>

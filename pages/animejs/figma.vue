d64955887921a49b3b6ca94dbd7b629f3259ea24
kauzkoko <kamelkebab@tutanota.com>
2024-02-03 17:44:11 +0100

    feat: add some animation graphics

---
+++ pages/animejs/figma.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <div>
+      <div
+        class="Frame2"
+        style="
+          width: 196px;
+          height: 280px;
+          position: relative;
+          background: white;
+        "
+      >
+        <div
+          class="Ellipse1"
+          style="
+            width: 60.7px;
+            height: 60.7px;
+            left: 67.65px;
+            top: 109.65px;
+            position: absolute;
+            mix-blend-mode: difference;
+            background: linear-gradient(180deg, black 0%, #00ff29 100%);
+            border-radius: 9999px;
+            border: 12px #0500ff solid;
+          "
+        ></div>
+        <div
+          class="Rectangle1"
+          style="
+            width: 30.35px;
+            height: 60.7px;
+            left: 67.65px;
+            top: 109.65px;
+            position: absolute;
+            mix-blend-mode: difference;
+            background: #d9d9d9;
+          "
+        ></div>
+        <div
+          class="Line1"
+          style="
+            width: 31.36px;
+            height: 0px;
+            left: 93.47px;
+            top: 170.35px;
+            position: absolute;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            mix-blend-mode: difference;
+            border: 1px white solid;
+          "
+        ></div>
+        <div
+          class="Line5"
+          style="
+            width: 31.36px;
+            height: 0px;
+            left: 99px;
+            top: 201.71px;
+            position: absolute;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            mix-blend-mode: difference;
+            border: 1px white solid;
+          "
+        ></div>
+        <div
+          class="Line3"
+          style="
+            width: 45.02px;
+            height: 0px;
+            left: 108.14px;
+            top: 154.67px;
+            position: absolute;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            mix-blend-mode: difference;
+            border: 1px white solid;
+          "
+        ></div>
+        <div
+          class="Line2"
+          style="
+            width: 31.36px;
+            height: 0px;
+            left: 86.3px;
+            top: 170.35px;
+            position: absolute;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            mix-blend-mode: difference;
+            border: 1px white solid;
+          "
+        ></div>
+        <div
+          class="Line4"
+          style="
+            width: 31.36px;
+            height: 0px;
+            left: 86.3px;
+            top: 201.71px;
+            position: absolute;
+            transform: rotate(-90deg);
+            transform-origin: 0 0;
+            mix-blend-mode: difference;
+            border: 1px white solid;
+          "
+        ></div>
+        <div
+          class="Rectangle2"
+          style="
+            width: 7.23px;
+            height: 50.08px;
+            left: 108.14px;
+            top: 109.65px;
+            position: absolute;
+            mix-blend-mode: difference;
+            background: linear-gradient(180deg, #ff0000 0%, #e28585 100%);
+          "
+        ></div>
+        <div
+          class="Rectangle3"
+          style="
+            width: 63.79px;
+            height: 40.69px;
+            left: 111.76px;
+            top: 94px;
+            position: absolute;
+            mix-blend-mode: difference;
+            background: #d9d9d9;
+          "
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/animejs/freshfigma.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper flexCenter bg-red">
+    <div
+      class="circle w-100vh mix-blend-difference bg-gradient-to-tr from-green-500 to-red-500 flexCenter"
+    >
+      <div
+        class="circle w-80% mix-blend-difference bg-gradient-to-tr from-blue-500 to-green-500 grid grid-cols-9"
+      >
+        <div
+          v-for="n in 81"
+          class="aspect-1 bg-gradient-to-tr from-blue-500 to-green-500 mix-blend-difference"
+        >
+          <div v-for="n in Math.floor(Math.random() * 3)" class="line"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { animate, stagger } from "@juliangarnierorg/anime-beta";
+
+onMounted(() => {
+  animate(".line", {
+    translateX: 320,
+    rotate: { from: -180 },
+    duration: 1250,
+    delay: stagger(65, { from: "center" }),
+    ease: "inOutQuint",
+    loop: true,
+    alternate: true,
+  });
+});
+</script>
